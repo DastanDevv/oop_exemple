50f64e208bf5d896eb388c6390b0dcd8a4ccdfa9
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2024-02-02 21:55:56 +0600

    default constr exemple

--- bin/default_constructor.dart
+++ bin/default_constructor.dart
@@ -1,6 +1,4 @@
-void main() {
-  Laptop acer = Laptop();
-}
+void main() {}
 
 class Laptop {
   String? brend;
@@ -16,4 +14,28 @@ class Student {
   int? age;
   String? schoolName;
   String? grande;
+}
+
+class User {
+  // 1.1)Defaut constructor болсо
+// дефолту бар мисалы эгерде бербесе ушул деп кетет.мисалы биздин
+//Userибиз нация деген жерди жазбаса по умолчанию кыргыз деп кетсин
+  User(
+    this.name,
+    this.finalName,
+    this.age,
+    this.profession,
+    this.password, [
+    this.nation = 'Kyrgyz', //здесь по умолчанию будет кыргыз
+    this.tuulganJer =
+        'Kyrgyzstan', //tuulganJer будет тоже по умолчанию Kyrgyzstan
+  ]);
+
+  final String name;
+  final String finalName;
+  final int age;
+  final String nation;
+  final String profession;
+  final String tuulganJer;
+  final String password;
 }
